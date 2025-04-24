05f01fa8695a81e5b9f441d35f5497c80f021b4c
uynvu078 <100883567+uynvu078@users.noreply.github.com>
2025-04-24 15:39:14 -0500

    Updated Styles

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function toggleProject(button) {
+  const allCards = document.querySelectorAll('.project-card');
+
+  allCards.forEach(card => {
+    if (card !== button.closest('.project-card')) {
+      card.classList.remove('open');
+    }
+  });
+
+  button.closest('.project-card').classList.toggle('open');
+}
